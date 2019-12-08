8652a773deaa5ab7a9987c0e73ec1fb2cb3d5516
Rupreet <rupreetg@outlook.com>
2019-12-08 17:35:10 +0530

    Fixed quantum container. Working now

---
+++ quantum/post-deployment-scripts.sh
@@ -0,0 +1,9 @@
+PATH="$PATH:/root/.dotnet/tools"
+PATH="$PATH:/home/vscode/.dotnet/tools"
+
+dotnet tool install -g Microsoft.Quantum.IQSharp
+
+PATH="$PATH:/home/vscode/.dotnet/tools"
+
+dotnet iqsharp install --user
+
